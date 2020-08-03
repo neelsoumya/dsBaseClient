62c42131e985fd18d90a46fba7e46ceefba166fb
neelsoumya <neel.soumya@gmail.com>
2020-08-03 18:35:09 +0100

    testing wip

---
+++ tests/testthat/test-smk-ds.retStr.R
@@ -0,0 +1,69 @@
+#-------------------------------------------------------------------------------
+# Copyright (c) 2019-2020 University of Newcastle upon Tyne. All rights reserved.
+#
+# This program and the accompanying materials
+# are made available under the terms of the GNU Public License v3.0.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#-------------------------------------------------------------------------------
+
+#
+# Set up
+#
+
+context("ds.dim::smk::setup")
+
+connect.studies.dataset.cnsim(list("LAB_TSC"))
+
+test_that("setup", {
+    ds_expect_variables(c("D"))
+})
+
+#
+# Tests
+#
+
+context("ds.dim::smk")
+test_that("simple dim, both", {
+    dim.res <- ds.dim('D', type='both')
+
+    expect_length(dim.res, 4)
+    expect_length(dim.res$`dimensions of D in sim1`, 2)
+    expect_equal(dim.res$`dimensions of D in combined studies`[[1]], 9379)
+    expect_equal(dim.res$`dimensions of D in combined studies`[[2]], 1)
+})
+
+context("ds.dim::smk")
+test_that("simple dim, split", {
+    dim.res <- ds.dim('D', type='split')
+
+    expect_length(dim.res, 3)
+    expect_length(dim.res$`dimensions of D in sim1`, 2)
+    expect_equal(dim.res$`dimensions of D in sim1`[[1]], 2163)
+    expect_equal(dim.res$`dimensions of D in sim3`[[2]], 1)
+})
+
+context("ds.dim::smk")
+test_that("simple dim, combine", {
+    dim.res <- ds.dim('D', type='combine')
+
+    expect_length(dim.res, 1)
+    expect_length(dim.res$`dimensions of D in combined studies`, 2)
+    expect_equal(dim.res$`dimensions of D in combined studies`[[1]], 9379)
+    expect_equal(dim.res$`dimensions of D in combined studies`[[2]], 1)
+})
+
+#
+# Done
+#
+
+context("ds.dim::smk::shutdown")
+
+test_that("shutdown", {
+    ds_expect_variables(c("D"))
+})
+
+disconnect.studies.dataset.cnsim()
+
+context("ds.dim::smk::done")
